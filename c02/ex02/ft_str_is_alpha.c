6eb5bc32d8093efaa54cd7bbf26a31e902adc84c
SANTOOS00 <mohamederraiss@icloud.com>
2025-05-17 00:21:26 +0100

    ma3lina :)

--- c02/ex02/ft_str_is_alpha.c
+++ c02/ex02/ft_str_is_alpha.c
@@ -2,10 +2,24 @@
 #include <stdio.h>
 int ft_str_is_alpha(char *str)
 {
-    
+    int i = 0;
+    while (str[i] != '\0')
+    {
+        if(!(str[i] >= 'a' &&str[i] <= 'z')
+        ||(str[i] >= 'A' && str[i] <= 'Z'))
+        {
+            return 0;
+        }
+        i++;
+    }
+    return 1;
 }
 int main(void)
 {
+    char str[100] = "erri11rfj";
+    ft_str_is_alpha(str);
+
+    printf ("%d",ft_str_is_alpha(str));
 
     return 0;
 }
